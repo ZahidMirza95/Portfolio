596b7da72ea7f69fdc8b48977f1c4c596545bb78
Zamzam95-Coder <zam19@rogers.com>
2021-04-15 00:27:28 -0400

    updated colors, added new project, changed description

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ import findMyCity from './images/FindMyCity_HeatMap.png';
 import gogogirl from './images/Gogogirl_Game_Real.png';
 import stockify from './images/Stockify_Main.png';
 import covidai from './images/COVIDAI_App.png';
+import hobbyhub from './images/hobbyhubquest.jpg';
+import hobbyhublogo from './images/hobbyhub.png';
 
 function App() {
 
@@ -109,20 +111,20 @@ function App() {
           <div className = "about"> 
             <h1 data-aos= "fade-in" data-aos-duration="2000"> ABOUT ME </h1>
             <p data-aos = "fade-up" data-aos-duration="1000"> Hello there! My name is Zahid Mirza and I am a Software Engineering & Management student
-            at McMaster University. Developing software is a huge passion of mine! I really enjoy learning 
-            new programming languages and solving problems using code. I'm also very passionate about web 
-            development, video game development, and AI! In my spare time I work on 
-            personal coding projects, play golf, and play video games! </p>
-            <div className = "rowImages">
-              <div className = "column">
+            at McMaster University. I really enjoy learning 
+            new programming languages and solving problems using code. I'm very passionate about both web 
+            development and video game development. I also have a strong interest in AI and RPA. In my spare time I work on 
+            personal coding projects, play golf, and play video games. </p>
+            <div className = "row">
+              <div className = "col">
                 <a href = "https://www.linkedin.com/in/mirzazahid/"><img src = {linkedIn} alt = "linkedIn" className = "linkedIn"></img></a>
                 <h2> LinkedIn </h2>
               </div>
-              <div className = "column">
+              <div className = "col">
                 <a href = "https://github.com/Zamzam95-coder"><img src = {github} alt = "github" className = "github"></img></a>
                 <h2> GitHub </h2>
               </div>
-              <div className = "column">
+              <div className = "col">
                 <a href = "https://drive.google.com/file/d/1jXaHOpxwb9qJU-K3i38t3teJLsnWUld9/view?usp=sharing"><img src = {resume} alt = "resume" className = "resume"></img></a>
                 <h2> Resume </h2>
               </div>
@@ -161,17 +163,29 @@ function App() {
             <h1 data-aos= "fade-in" data-aos-duration="2000"> PROJECTS </h1>
               <div className = "container">
                 <div className = "row">
+                <div className = "col-lg">
+                    <Project name = "HobbyHub"
+                    date = "Jan. 2021"
+                    desc = "A web application that provides a hobby for the user to try out by analyzing their responses, using the K-Means clustering algorithm, to various questions based on their personality. The application also provides them with some other users to connect with so that they can learn the hobby together. Created using the React.js library and Bootstrap."
+                    img = {hobbyhub}
+                    imgName = "hobbyhub"
+                    logos = {[githubLogo, devpostLogo]}
+                    links = {["https://github.com/minericpark/hobbyapp", "https://devpost.com/software/hobbyhub"]}
+                    />
+                  </div>
                   <div className = "col-lg">
                     <Project name = "Portfolio"
                     date = "Dec. 2020 - Present"
-                    desc = "A portfolio that shows off some more information about me and the various projects that I've worked on. Currently on the first version of the website, plan to update it even further later on. Created using HTML, CSS, Bootstrap, and JavaScript with the React.js library."
+                    desc = "A portfolio that shows off some more information about me and the various projects that I've worked on. Currently on the second version (minor update from the first) of the website, plan to update it even further later on. Created using HTML, CSS, Bootstrap, and JavaScript with the React.js library."
                     img = {portfolio}
                     imgName = "portfolio"
                     logos = {[githubLogo]}
                     links = {["https://github.com/Zamzam95-coder/Portfolio"]}
                     />
                   </div>
-                  <div className = "col-lg">
+                </div>
+                <div className = "row">
+                <div className = "col-lg">
                     <Project name = "Cosmic Samurai" 
                     date = "Nov. 2020 - Dec. 2020"
                     img = {cosmic}
@@ -181,8 +195,6 @@ function App() {
                     links = {["https://github.com/Zamzam95-coder/Cosmic-Samurai", "https://itch.io/jam/game-off-2020/rate/840159"]}
                     />
                   </div>
-                </div>
-                <div className = "row">
                   <div className = "col-lg">
                     <Project name = "FindMyCity"
                     date = "Oct. 2020"
@@ -193,18 +205,18 @@ function App() {
                     links = {["https://github.com/Zamzam95-coder/FindMyCity", "https://devpost.com/software/find-my-city"]}
                     />
                   </div>
-                  <div className = "col-lg">
-                    <Project name = "Go Go Girl"
-                    date = "Sept. 2020"
-                    desc = "Video game developed in a team of four, within 36 hours, for ShellHacks 2020. Purpose of this game is to show the impact social media can have on mental health. Created using Unity and C#."
-                    img = {gogogirl}
-                    imgName = "gogogirl"
-                    logos = {[githubLogo, devpostLogo]}
-                    links = {["https://github.com/Zamzam95-coder/GoGoGirl", "https://devpost.com/software/go-go-girl"]}
-                    />
-                  </div>
                 </div>
                 <div className = "row">
+                  <div className = "col-lg">
+                      <Project name = "Go Go Girl"
+                      date = "Sept. 2020"
+                      desc = "Video game developed in a team of four, within 36 hours, for ShellHacks 2020. Purpose of this game is to show the impact social media can have on mental health. Created using Unity and C#."
+                      img = {gogogirl}
+                      imgName = "gogogirl"
+                      logos = {[githubLogo, devpostLogo]}
+                      links = {["https://github.com/Zamzam95-coder/GoGoGirl", "https://devpost.com/software/go-go-girl"]}
+                      />
+                  </div>
                   <div className = "col-lg">
                     <Project name = "Stockify"
                     date = "May 2020"
@@ -215,19 +227,9 @@ function App() {
                     links = {["https://github.com/Zamzam95-coder/Stockify", "https://devpost.com/software/stocker-kw70ue"]}
                     />
                   </div>
-                  <div className = "col-lg">
-                    <Project name = "COVID-AI"
-                    date = "Feb. 2020 - Mar. 2020"
-                    desc = "Web application created in a team of four that diagnoses COVID-19 using a machine-learning algorithm. User inputs responses to various questions within the web app, and is then suggested the best course of action based on the results of the machine learning algorithm. Web app was created using HTML, CSS, and JavaScript, and the machine-learning algorithm was made using IBM Watson."
-                    img = {covidai}
-                    imgName = "covidai"
-                    logos = {[githubLogo]}
-                    links = {["https://github.com/Zamzam95-coder/HackTheGlobe2020"]}
-                    />
-                  </div>
                 </div>
                 <h1> Built by Zahid Mirza </h1>
-                <h2> Version 1.0 </h2>
+                <h2> Version 1.1 </h2>
               </div>
             </div>
         </section>
